69977874ddca6664ae85491b15cefbb1ec27aba4
Yhon L. Castañeda <leytoncastaneda@gmail.com>
2025-02-26 22:05:32 -0500

    adding new fiels files db

--- vectorial_db/sql/init.sql
+++ vectorial_db/sql/init.sql
@@ -4,11 +4,12 @@ CREATE SCHEMA IF NOT EXISTS vectorial;
 -- Set the search path to the new schema (vectorial by default)
 SET search_path TO vectorial, public;
 
--- Create the extension in the new schema
+-- Install the vector extension in the new schema
 CREATE EXTENSION IF NOT EXISTS vector SCHEMA vectorial;
 
 -- Create the table in the new schema
 CREATE TABLE session_embeddings (
     id_session INT NOT NULL,  -- Unique identifier for each session
-    embedding vector(1536) NOT NULL  -- Column to store vector embeddings
+    texto TEXT NOT NULL,      -- Column to store the text of the session
+    embedding vector NOT NULL  -- Column to store vector embeddings
 );
