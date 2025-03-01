25eedbd6606b4c263b12f06ebb5d70b5f1f52e79
Yhon L. Castañeda <leytoncastaneda@gmail.com>
2025-03-01 14:00:22 -0500

    augment configuration

--- augment/aumentador/path.py
+++ augment/aumentador/path.py
@@ -1,29 +1,33 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, status, HTTPException
 from service.api import post_searching
 from . import schema
+from .question import structure_question
 import logging
 
 # Define the FastAPI router
 router = APIRouter(
     prefix="/augment",
-    tags=["aument"]
+    tags=["augment"]
 )
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
 # Define the POST method
-@router.post("/aumentador", status_code=status.HTTP_201_CREATED)
+@router.post("/aumentador",response_model=schema.AugmentResponse , status_code=status.HTTP_201_CREATED)
 async def augment_search(request: schema.AugmentRequest):
     try:
 
         # Log the received string
         logger.info(f"Received string: {request.text}")
 
+        # make question
+        pregunta = structure_question(request.text, request.prompt)
+        #pregunta = "Basado en este texto: " + request.text + " Responde a la siguiente pregunta: " + request.prompt
+
         # make searching
-        make_searching = post_searching(request.text, request.prompt)
-        augment_reponse = schema.AugmentResponse(question=make_searching)
-        response_llm = augment_reponse.model_dump(exclude_none=True)
+        augment_reponse = schema.AugmentResponse(question=pregunta)
+        response_llm = post_searching(augment_reponse.model_dump(exclude_none=True))
         print(response_llm)
         return response_llm
           
