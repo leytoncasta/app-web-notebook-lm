8ca67a32e1a1db810de816b708ca54f5b3198bbf
ErichGiusseppe <eg.soto@uniandes.edu.co>
2025-04-06 18:14:17 -0500

    cambios en el string de coneccion

--- embeddings/database/db.py
+++ embeddings/database/db.py
@@ -9,7 +9,7 @@ ABSOLUTE_PATH = Path(__file__).resolve().parent
 env_file = ABSOLUTE_PATH / '.env'
 #load_dotenv(env_file)
 
-CONNECTION_STRING = "postgresql+psycopg2://team5:Project2025!@10.109.1.19:532/files"
+CONNECTION_STRING = "postgresql+psycopg2://team5:Project2025!@10.248.32.6:5432/postgres"
 
 engine = create_engine(
     CONNECTION_STRING,
@@ -17,7 +17,7 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+;
 Base = declarative_base()
 
 def get_db():
