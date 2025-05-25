9719c17e6de5697ac23b07635774b3be91020bbb
Yhon L. Castañeda <leytoncastaneda@gmail.com>
2025-05-25 09:51:58 -0500

    deleting foreing key chat id

--- backend/gemini/database/table_prompt_response.sql
+++ backend/gemini/database/table_prompt_response.sql
@@ -4,6 +4,5 @@ CREATE TABLE prompt_response (
     message TEXT,
     chat_id VARCHAR(255) NOT NULL,
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
-    status_code INTEGER NOT NULL,
-    PRIMARY KEY (chat_id)
+    status_code INTEGER NOT NULL
 );
--- backend/gemini/model.py
+++ backend/gemini/model.py
@@ -5,6 +5,6 @@ class PromptResponseDB(Base):
     __tablename__ = "prompt_response"
 
     message = Column(Text, nullable=False)
-    chat_id = Column(Integer, primary_key=True, nullable=False)
+    chat_id = Column(Integer, nullable=False)
     status_code = Column(Integer, nullable=False)
     date = Column(TIMESTAMP, nullable=False)
